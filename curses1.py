a05a473a46b0dba9f3bc4279f201d5489cc888f9
Ed Skoviak <eskoviak@gmail.com>
2012-12-30 02:58:23 -0500

    adding curses

---
+++ curses1.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+
+import curses
+import curses.textpad
+import time
+
+stdscr = curses.initscr()
+
+#curses.noecho()
+curses.echo()
+
+# the following are used in the Textpad
+#begin_x = 20
+#begin_y = 7
+#height = 5
+#width = 40
+
+#win = curses.newwin(height, width, begin_y, begin_x)
+
+# The following two lines use the curses.textpad object
+#tb = curses.textpad.Textbox(win)
+#text = tb.edit()
+
+# The following act on the window object directly
+str_list = []
+str_list.append('Hi ')
+stdscr.border()
+stdscr.addstr(4,1,'Enter your name:  ')
+input = stdscr.getstr(5,1)
+str_list.append(str(input, 'utf-8'))
+stdscr.addstr(6,1,''.join(str_list))
+
+while 1:
+    c = stdscr.getch()
+    if c == ord('q'):
+        break
+    elif c == curses.KEY_HOME:
+        x=y=0
+
+
+curses.endwin()
