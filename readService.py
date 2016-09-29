a540d223865226c54c09968e65f15aab01e779d4
opr1073 <edmund.skoviak@infarmbureau.com>
2016-09-29 11:06:01 -0400

    changed to useing file as output

--- readService.py
+++ readService.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 
 import json
+import urllib
 
 # TODO Get Environment from user
-url = 'http://ap_karafb_dev:8181/api/policies/'
+ENVB = 'http://ap-karafb-dev:8181/api/policies/'
 
-# TODO Get Policy number
+# Get Policy number; replace space with '+'
+policyNumber = raw_input('Enter Policy Number: ').replace(' ', '+')
+
+print ('Call Url: ' + ENVB+ policyNumber)
+#exit(0)
 
 # Dummy code for testing
-fp = open('users.json', 'r')
+#fp = open('users.json', 'r')
 
-#print fp.read()
-print json.dumps(json.loads(fp.read()),indent=4)
+data = urllib.urlopen(ENVB+policyNumber).read()
+outfile = open('output.dat', 'w')
 
+print ('Writing to output.dat: '+ str(len(data)) + ' bytes')
+outfile.write(json.dumps(json.loads(data),indent=4))
+outfile.close()
