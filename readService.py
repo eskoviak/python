f6ca9ed3853a88048fc2650a2a42ea8781211fbf
opr1073 <edmund.skoviak@infarmbureau.com>
2016-09-30 12:58:57 -0400

    updated reader; works off line use output.dat

--- readService.py
+++ readService.py
@@ -9,15 +9,31 @@ ENVB = 'http://ap-karafb-dev:8181/api/policies/'
 # Get Policy number; replace space with '+'
 policyNumber = raw_input('Enter Policy Number: ').replace(' ', '+')
 
-print ('Call Url: ' + ENVB+ policyNumber)
-#exit(0)
+if policyNumber != 'output.dat':
+  print ('Call Url: ' + ENVB+ policyNumber)
 
-# Dummy code for testing
-#fp = open('users.json', 'r')
+  data = urllib.urlopen(ENVB+policyNumber).read()
+  dataJSON = json.loads(data)
 
-data = urllib.urlopen(ENVB+policyNumber).read()
-outfile = open('output.dat', 'w')
+  # Write data to file for off line testing and begin exploring the DOM
+  outfile = open('output.dat', 'w')
+  print ('Writing to output.dat: '+ str(len(data)) + ' bytes')
+  outfile.write(json.dumps(dataJSON,indent=4))
+  outfile.close()
+else:
+  infile = open('output.dat', 'r')
+  dataJSON = json.loads(infile.read())
+  infile.close()
 
-print ('Writing to output.dat: '+ str(len(data)) + ' bytes')
-outfile.write(json.dumps(json.loads(data),indent=4))
-outfile.close()
+# What do we have?
+print ('Key')
+for keys in dataJSON.keys():
+  print keys
+  print '====='
+  print dataJSON[keys]
+  #for keys1 in (dataJSON[keys]).keys():
+  #  print keys1
+  #print ('  Children')
+  #for key in keys:
+    #print ('  ' + dataJSON[key])
+    #print key
