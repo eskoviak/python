bbeb8b66b2d94134a941bb273f28a53410a769e0
eskoviak <eskoviak@gmail.com>
2016-10-22 09:51:52 -0400

    update from MBP

--- readDatastore.py
+++ readDatastore.py
@@ -1,9 +1,24 @@
 # File readDatastore.py
 from google.cloud import datastore
+import json
+
+
 
 def create_client(project_id):
     return datastore.Client(project_id)
 
-client = create_client('5629499534213120')
+def get_obs(client):
+    query = client.query(kind='data')
 
-print(client)
+    return list(query.fetch())
+
+
+if __name__ == '__main__':
+    project_id = '5629499534213120'
+
+    client = create_client(project_id)
+    obs = get_obs(client)
+    for item in obs:
+        print json.loads(item)
+
+        
