1c42fda9536cd8fd09b525cf4248f051ddeee2de
eskoviak <eskoviak@gmail.com>
2016-10-23 20:39:29 -0400

    Updated from MBP.  See issues.

--- readDatastore.py
+++ readDatastore.py
@@ -1,24 +1,32 @@
+#!/usr/bin/env python
+
 # File readDatastore.py
+
 from google.cloud import datastore
 import json
 
+# Setup credentials
+from oauth2client.client import GoogleCredentials
+credentials = GoogleCredentials.get_application_default()
 
 
-def create_client(project_id):
-    return datastore.Client(project_id)
+def create_client(project_id, namespace):
+    return datastore.Client(project_id, namespace, credentials)
 
 def get_obs(client):
-    query = client.query(kind='data')
+    query = client.query(kind='healthFact')
 
     return list(query.fetch())
 
 
 if __name__ == '__main__':
-    project_id = '5629499534213120'
+    project_id = 'spartan-thunder-130321'
+    namespace = 'myhealth'
 
-    client = create_client(project_id)
+    client = create_client(project_id, namespace)
     obs = get_obs(client)
-    for item in obs:
-        print json.loads(item)
+    # for item in obs:
+    #     print json.loads(item)
+    print len(obs)
 
         
