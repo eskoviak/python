02bb6d35116f93c46dc3e9d9a27a18fb7ffee06e
eskoviak <eskoviak@gmail.com>
2016-11-04 17:19:14 +0000

    midday Gulch

--- searchUSDA.py
+++ searchUSDA.py
@@ -7,6 +7,8 @@ import readConfig
 
 
 searchURITemplate='http://api.nal.usda.gov/ndb/search/?format=json&q={0}&sort=n&max=25&offset=0&api_key={1}'
+reportURITemplate='http://api.nal.usda.gov/ndb/reports/?ndbno={0}&type=f&format=json&api_key={1}'
+debug = True
 
 def loadFile(filename):
   if(os.path.exsists(filename)):
@@ -17,42 +19,61 @@ def loadFile(filename):
   else:
     return null
 
+def searchUSDA(criteria):
+  choice.replace(' ', '%20')
+  key = readConfig.readKey('configuration.cfg', 'api.data.key.gov')
+  url = searchURITemplate.format(choice,key)
+  if(debug):
+    print("Search URI: " + url)
+
+  uriSocket = urllib.urlopen(url)
+  respCode = uriSocket.getcode()
+  if (respCode == 200):
+    dataJSON = json.loads(uriSocket.read())
+    return(respCode, dataJSON)
+  else:
+    return(respCode, "")
+
+def reportUSDA(ndbno, type):
+  key = readConfig.readKey('configuration.cfg', 'api.data.key.gov')
+  url = reportURITemplate.format(ndbno,key)
+  if(debug):
+    print("Search URI: " + url)
+
+  uriSocket = urllib.urlopen(url)
+  respCode = uriSocket.getcode()
+  if (respCode == 200):
+    dataJSON = json.loads(uriSocket.read())
+    return(respCode, dataJSON)
+  else:
+    return(respCode, "")
+
 if __name__ == '__main__':
 
   # Get user's choice
   choice = ""
   while choice == "":
     choice = raw_input('Enter search term: ')
-
-  choice.replace(' ', '%20')
-  key = readConfig.readKey('configuration.cfg', 'api.data.key.gov')
-  url = searchURITemplate.format(choice,key)
-  print ('Call Url: ' + url)
   
-  uriSocket = urllib.urlopen(url)
-  if (uriSocket.getcode() == 200):
-    data=uriSocket.read()
-    dataJSON = json.loads(data)
-
+  (code, dataJSON) = searchUSDA(choice)
+  if(debug):  
     print("For search term: " + choice)
     print("Items Found: ")
     print(dataJSON['list']['end'])
 
+  if(code == 200):
     for item in dataJSON['list']['item']:
       print(item['ndbno'] + " : " + item['name'])
+    choice=raw_input('Enter NDBNO to report: ')
+    (code, dataJSON) = reportUSDA(choice, 'f')
+  else:
+    print("Error in request: " + str(code))
+    exit(-1)
 
+  if(debug):
     # Write data to file
     outfile = open('usda.dat', 'w')
-    print ('Writing to usda.dat: '+ str(len(data)) + ' bytes')
+    print ('Writing to usda.dat: '+ str(len(dataJSON)) + ' bytes')
     outfile.write(json.dumps(dataJSON,indent=4))
     outfile.close()
   
-    choice = raw_input('Output written to usda.dat.  View raw data (y/N)? ')
-    if choice == 'y'.upper():
-      print ('Key')
-      for keys in dataJSON.keys():
-        print keys
-        print '====='
-        print dataJSON[keys]
-  else:
-    print("Error in request: " + str(uriSocket.getcode()))
