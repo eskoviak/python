34fce7d5ea5789c2af687713e441b60c46bb0797
opr1073 <edmund.skoviak@infarmbureau.com>
2016-11-16 10:05:57 -0500

    morning update opr

--- readService.py
+++ readService.py
@@ -7,31 +7,36 @@ import os
 environments = {
     'ENVA' : 'http://ap-karafa-dev:8181/api/policies/',
     'ENVB' : 'http://ap-karafb-dev:8181/api/policies/',
-    'localhost' : 'http://localhost:8181/api/policies/'
+    'localhost' : 'http://localhost:8181/api/policies/',
+    'QAG' : 'https://karaf-g.infarmbureau.com:8443/api/policies/'
     'ESB' : 'http://esb2-dev.infarmbureau.com:9081/'}
 
 def loadFile(filename):
-  infile = open(filename, 'r')
-  fileData = json.loads(infile.read())
-  infile.close()
-  return fileData
+    try:
+        infile = open(filename, 'r')
+        fileData = json.loads(infile.read())
+        infile.close()
+        return fileData
+    catch IOError as ioe:
+        print(ioe.message)
 
 if __name__ == '__main__':
 
-  # Get user's choice
-  choice = raw_input('Enter (U)rl or (F)ile name: (U/f): ')
-  if choice == "": 
-    choice = 'U'
+    # Get user's choice
+    choice = raw_input('Enter (U)rl or (F)ile name: (U/f): ')
+    if choice == None:
+        print('Using URL') 
+        choice = 'U'
 
-  if choice == 'U' or choice == 'u':
-    while True:
-      print('Choose environment:')
-      for key in environments.keys():
-        print(key)
-      inKey = raw_input('--> ')
-      if (inKey in environments):
-        break
-      else:
+    if choice == 'U' or choice == 'u':
+        while True:
+            print('Choose environment:')
+            for key in environments.keys():
+            print(key)
+            inKey = raw_input('--> ')
+            if (inKey in environments):
+            break
+    else:
         print("Not in list.  Try again")
 
     if inKey == 'ESB':
@@ -40,25 +45,25 @@ if __name__ == '__main__':
         catch:
             print('Exception in ESB')
             
-    # Get Policy number; replace space with '+'
+    # Get Policy number; replace space with '%20'
     policyNumber = raw_input('Enter Policy Number: ').replace(" ", "%20").upper()
     url = environments[inKey]+policyNumber;
+    if(inKey == 'localhost'):
+        # TODO add suffix based on policy type
+        pass
+        
     print ('Call Url: ' + url)
-    data = urllib.urlopen(url).read()
-    fileData = json.loads(data)
+    response = urllib.urlopen(url).read()
+    fileData = json.loads(response)
 
-    # Write data to file
+    # Write response to file
     outfile = open('output.dat', 'w')
-    print ('Writing to output.dat: '+ str(len(data)) + ' bytes')
+    print ('Writing to output.dat: '+ str(len(response)) + ' bytes')
     outfile.write(json.dumps(fileData,indent=4))
     outfile.close()
-    choice = raw_input('Output written to output.dat.  View raw data (y/N)? ')
+    choice = raw_input('Output written to output.dat.  View file on console (y/N)? ')
     if choice == "y".upper():
-      print ('Key')
-      for keys in fileData.keys():
-        print keys
-        print '====='
-        print fileData[keys]
+
   else:
     filename = raw_input('Enter filename: (./output.dat)')
     if filename == "":
