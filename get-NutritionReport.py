55743fd40fa323f6b5695bfdcf260c180acecdd5
Ed Skoviak <eskoviak@gmail.com>
2018-08-24 20:18:29 -0500

    Added getMeasures

--- get-NutritionReport.py
+++ get-NutritionReport.py
@@ -29,17 +29,26 @@ def reportUSDA(ndbno, type):
 def getNutrientItem(nutrients, nutrientId):
     if(len(nutrients) > 0):
         for nutrient in nutrients:
-            if(nutrient['nutrient_id'] == nutrientId):
+            if(int(nutrient['nutrient_id']) == int(nutrientId)):
                 return nutrient
-
     return ''
 
+def getNutrientMeasures(nutrients):
+    if(len(nutrients) > 0):
+        for nutrient in nutrients:
+            if(int(nutrient['nutrient_id']) == 208):
+                return nutrient['measures']
+        return ''
+    else:
+        return ''
+    
 # Globals
 nutrientList = dict(Energy='208', Fat='204', Carbs='205', Protein='203')
 
 
 if(__name__ == '__main__'):
-    test_ndbno = '45057514'
+    #test_ndbno = '45057514'
+    test_ndbno = '09040' #bananas, raw
     ratio = .32
     (ret,jsonResp) = reportUSDA(test_ndbno, 'f')
     if(debug):
@@ -47,17 +56,19 @@ if(__name__ == '__main__'):
 
     print(jsonResp['report']['food']['name'])
     print(str.format("Per {0} grams:", 100 * ratio))
-    nutrientKeys = nutrientList.keys()
     nutrients=jsonResp['report']['food']['nutrients']   #should be the nutrients dict
+    if(debug):
+        print(nutrients)
     #print(getNutrientItem(nutrients, '208'))
     print('Nutrient\tValue\tUnits')
-    for key in nutrientKeys:
-        nutrientDetail = getNutrientItem(nutrients, nutrientList[key])
+    for value in nutrientList.values():
+        nutrientDetail = getNutrientItem(nutrients, value)
 
         if(len(nutrientDetail) > 0):
             print(str.format("{0}\t\t{1}\t{2}",
                 nutrientDetail['name'],
                 float(nutrientDetail['value']) * ratio,
                 nutrientDetail['unit']))
+    print(getNutrientMeasures(nutrients))
 
   
