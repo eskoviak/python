f8f7399e1df736fb26b4644f43e50841bba21266
Ed <eskoviak@eskoviak.com>
2020-06-03 08:43:33 -0500

    updates from mlenv

--- mlvenv/svr_regression.py
+++ mlvenv/svr_regression.py
@@ -23,3 +23,21 @@ regressor.fit(X, Y)
 
 # Predict new value (mind the scaling)
 print(sc_Y.inverse_transform(regressor.predict(sc_X.transform([[6.5]]))))
+
+# Visualising the SVR results
+plt.scatter(sc_X.inverse_transform(X), sc_Y.inverse_transform(Y), color = 'red')
+plt.plot(sc_X.inverse_transform(X), sc_Y.inverse_transform(regressor.predict(X)), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+# X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
+# X_grid = X_grid.reshape((len(X_grid), 1))
+# plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+# plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid))), color = 'blue')
+# plt.title('Truth or Bluff (SVR)')
+# plt.xlabel('Position level')
+# plt.ylabel('Salary')
+# plt.show()
