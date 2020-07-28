3211a7dc6390a373021d263a67aa91364022b999
Ed <eskoviak@eskoviak.com>
2020-07-27 20:31:45 -0500

    made recursive

--- mlvenv/covid_curse.py
+++ mlvenv/covid_curse.py
@@ -4,115 +4,117 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
+import sys
 
-stdscr = curses.initscr()
-
-curses.noecho()
-curses.cbreak()
-stdscr.keypad(True)
-
-while 1:
-    scope='None'
-    stdscr.clear()
-    stdscr.border()
-    stdscr.addstr(4,1,'Report Types:  ')
-    stdscr.addstr(5,3, '1 -- Entire US')
-    stdscr.addstr(6,3, '2 -- State')
-    stdscr.addstr(7,3, '3 -- County (by FIPS)')
-    stdscr.addstr(9,1, 'Enter choice [1..3], q to exit  ')
-
-    curses.echo()    
-    c = stdscr.getch()
-
-    if c == ord('1'):
-        scope = 'US'
-        scopeValue = b'United States'
-        break
-    elif c == ord('2'):
-        scope = 'State'
-        stdscr.addstr(11,1, 'Enter the state: ')
-        #(y,x) = curses.getsyx()
-        scopeValue  = stdscr.getstr(12,1)
-        break
-    elif c == ord('3'):
-        scope = 'County'
-        stdscr.addstr(11,1, 'Enter the FIPS code for the county: ')
-        scopeValue = stdscr.getstr(12,1)
-        break
-    elif c != ord('q'):
-        continue
-
-curses.noecho
-if scope == 'None':
-    exit()
-scopeValue = str(scopeValue, 'utf-8')
-
-stdscr.clear()
-stdscr.addstr(4,1, 'Loading the dataset')
-
-# load the dataset
 dataset_confirmed = pd.read_csv('D:\\Source\\Repos\\COVID-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_confirmed_us.csv')
 dataset_death = pd.read_csv('D:\\Source\\Repos\\COVID-19\\csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_deaths_us.csv')
-
-# Array of column names from column 11 (L) to end
+dates = pd.to_datetime(dataset_confirmed.columns[11:].values)
 dateColNames = dataset_confirmed.columns[11:].values
-dates = pd.to_datetime(dateColNames)
 
-# Setup the plot
-months = mdates.MonthLocator()  # every month
-weeks = mdates.WeekdayLocator()
-months_fmt = mdates.DateFormatter('%Y-%m')
-fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
+def getSeries ( scope, scopeValue):
+    if scope == 'US':
+        confirmed = dataset_confirmed.loc[:, dateColNames]
+        cum_confirmed = confirmed.sum()
 
-# format the ticks
-ax1.xaxis.set_major_locator(months)
-ax1.xaxis.set_major_formatter(months_fmt)
-ax1.xaxis.set_minor_locator(weeks)
+        deaths = dataset_death.loc[:, dateColNames]
+        cum_deaths = deaths.sum()
 
-# Get y first so we know how many
-# ALl
-if scope == 'US':
-    confirmed = dataset_confirmed.loc[:, dateColNames]
-    cum_confirmed = confirmed.sum()
+        cum_rate = cum_deaths / cum_confirmed
 
-    deaths = dataset_death.loc[:, dateColNames]
-    cum_deaths = deaths.sum()
+        title = "US"
+    elif scope == 'State':
+        confirmed = dataset_confirmed.loc[dataset_confirmed['Province_State'] == scopeValue, dateColNames]
+        cum_confirmed = confirmed.sum()
 
-    cum_rate = cum_deaths / cum_confirmed
+        deaths = dataset_death.loc[dataset_confirmed['Province_State'] == scopeValue, dateColNames]
+        cum_deaths = deaths.sum()
 
-    title = "US"
-elif scope == 'State':
-    confirmed = dataset_confirmed.loc[dataset_confirmed['Province_State'] == scopeValue, dateColNames]
-    cum_confirmed = confirmed.sum()
+        cum_rate = cum_deaths / cum_confirmed
 
-    deaths = dataset_death.loc[dataset_confirmed['Province_State'] == scopeValue, dateColNames]
-    cum_deaths = deaths.sum()
+        title = scopeValue
+    elif scope == 'County':
+        confirmed = dataset_confirmed.loc[dataset_confirmed['FIPS'] == int(scopeValue), dateColNames]
+        cum_confirmed = confirmed.sum()
 
-    cum_rate = cum_deaths / cum_confirmed
+        deaths = dataset_death.loc[dataset_confirmed['FIPS'] == int(scopeValue), dateColNames]
+        cum_deaths = deaths.sum()
 
-    title = scopeValue
-elif scope == 'County':
-    confirmed = dataset_confirmed.loc[dataset_confirmed['FIPS'] == scopeValue, dateColNames]
-    cum_confirmed = confirmed.sum()
+        cum_rate = cum_deaths / cum_confirmed
 
-    deaths = dataset_death.loc[dataset_confirmed['FIPS'] == scopeValue, dateColNames]
-    cum_deaths = deaths.sum()
+        titledf = dataset_confirmed.loc[dataset_confirmed['FIPS'] == int(scopeValue)]
+        index = titledf.index[0]
+        title = str(dataset_confirmed.at[index, 'Combined_Key'])
 
-    cum_rate = cum_deaths / cum_confirmed
+    return(cum_confirmed, cum_deaths, cum_rate, title)
 
-    title = dataset_confirmed.at[dataset_confirmed['FIPS'] == scopeValue, 'Combined_Key']
+def plot (scope, scopeValue):
+    # Setup the plot
+    months = mdates.MonthLocator()  # every month
+    weeks = mdates.WeekdayLocator()
+    months_fmt = mdates.DateFormatter('%Y-%m')
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
 
-# Set the Date Axis
-ax1.stackplot(dates,cum_confirmed, cum_deaths, labels=('Confirmed', 'Death'), colors=('green', 'red'))
-ax1.legend()
-ax2.plot(dates, cum_rate, color='blue')
-plt.title(title, loc='center')
-plt.xlabel('Date')
-ax1.set_ylabel('Incidence')
-ax2.set_ylabel('Mobidity Rate')
-fig.autofmt_xdate()
-plt.show()
-curses.nocbreak()
-stdscr.keypad(False)
-curses.echo()
-curses.endwin()
+    # format the ticks
+    ax1.xaxis.set_major_locator(months)
+    ax1.xaxis.set_major_formatter(months_fmt)
+    ax1.xaxis.set_minor_locator(weeks)
+
+    (cum_confirmed, cum_deaths, cum_rate, title) = getSeries(scope, scopeValue)
+    ax1.stackplot(dates,cum_confirmed, cum_deaths, labels=('Confirmed', 'Death'), colors=('green', 'red'))
+    ax1.legend()
+    ax2.plot(dates, cum_rate, color='blue')
+    plt.title(title, loc='center')
+    plt.xlabel('Date')
+    ax1.set_ylabel('Incidence')
+    ax2.set_ylabel('Mobidity Rate')
+    fig.autofmt_xdate()
+    plt.show()
+
+
+#
+# MAIN
+#
+if __name__ == '__main__':
+    stdscr = curses.initscr()
+
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+
+
+    while 1:
+        scope='None'
+        stdscr.clear()
+        stdscr.border()
+        stdscr.addstr(4,1,'Report Types:  ')
+        stdscr.addstr(5,3, '1 -- Entire US')
+        stdscr.addstr(6,3, '2 -- State')
+        stdscr.addstr(7,3, '3 -- County (by FIPS)')
+        stdscr.addstr(9,1, 'Enter choice [1..3], q to exit  ')
+
+        curses.echo()    
+        c = stdscr.getch()
+
+        if c == ord('1'):
+            plot('US', str(b'United States', 'utf-8'))
+            continue
+        elif c == ord('2'):
+            scope = 'State'
+            stdscr.addstr(11,1, 'Enter the state: ')
+            #(y,x) = curses.getsyx()
+            scopeValue  = str(stdscr.getstr(12,1), 'utf-8')
+            break
+        elif c == ord('3'):
+            scope = 'County'
+            stdscr.addstr(11,1, 'Enter the FIPS code for the county: ')
+            scopeValue = str(stdscr.getstr(12,1), 'utf-8')
+            break
+        elif c != ord('q'):
+            curses.nocbreak()
+            stdscr.keypad(False)
+            curses.echo()
+            curses.endwin()
+            exit(0)
+    curses.noecho
+
+    # Set the Date Axis
